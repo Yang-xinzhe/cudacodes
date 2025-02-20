862faf8f8bbb37805866ca2d70548163407e98cf
Yangxinzhe <1245314699@qq.com>
2025-02-20 14:45:04 +0800

    feat: header add blockReduce api

--- mat_vec/include/coalesced_warpblock_sgemv.cuh
+++ mat_vec/include/coalesced_warpblock_sgemv.cuh
@@ -3,3 +3,7 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 #include <assert.h>
+
+__device__ __forceinline__ void blockReduceSum(float val, float *smem, int tid, int blockDimX);
+__global__ void coalesced_warpblock_sgemv_kernel(float* __restrict__ mat_gpu, float* __restrict__ vec_gpu, float* __restrict__ res_gpu, int M, int N);
+float run_kernel_coalesced_warpblock_sgemv(float* __restrict__ mat_gpu, float* __restrict__ vec_gpu, float* __restrict__ res_gpu, int M, int N);
